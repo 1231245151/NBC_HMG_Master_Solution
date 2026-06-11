31992072f6b90d64603f7815ff969de16b0dc5a6
1231245151 <homingi@naver.com>
2026-06-11 11:42:50 +0900

    오브젝트 풀링 과제

---
+++ Source/camera3dPrectice/Public/ObjectPool/ObjectPoolDataAsset.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ObjectPool/PooledObjectData.h"
+#include "ObjectPoolDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAMERA3DPRECTICE_API UObjectPoolDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    TArray<FPooledObjectData> PoolDataArray;
+};
